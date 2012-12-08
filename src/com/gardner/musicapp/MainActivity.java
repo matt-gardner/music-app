1a3f506f4c363e1d839ebafcb588ee27fa408eaf
Matt Gardner <mjg82@byu.edu>
2012-12-08 12:42:38 -0500

    Displaying frequencies now works
    I created a Microphone class as a separate thread, and a chart to display the
    result of an FFT.  After some tweaking, it looks like it works.

--- src/com/gardner/musicapp/MainActivity.java
+++ src/com/gardner/musicapp/MainActivity.java
@@ -1,115 +1,139 @@
 package com.gardner.musicapp;
 
-import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
-
-
 import android.app.Activity;
 import android.content.Intent;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends Activity
-{
-    public static final String TAG = "MusicApp";
+import java.util.ArrayList;
 
-    private int SAMPLE_RATE = 8000;
-    private int BUFFER_SIZE = 4 * 1024;;
-    DoubleFFT_1D fft = new DoubleFFT_1D(BUFFER_SIZE);
-    byte[] buffer;
-    TextView textView;
-    private AudioRecord mic;
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.XYSeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+public class MainActivity extends Activity {
+
+    public static final String TAG = "musicapp.Main";
+
+    private TextView textView;
+    private Microphone microphone;
+    private Handler handler;
+    private Runnable callback;
+
+    private Button switchMic;
+    private String micOn, micOff;
+
+    private XYSeries freqData;
+    private XYMultipleSeriesDataset dataset;
+    private XYMultipleSeriesRenderer renderer;
+    private XYSeriesRenderer seriesRenderer;
+    private GraphicalView graphicalView;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.main);
-        mic = findMicrophoneSettings();
+        handler = new Handler();
+        callback = new FrequencyUpdater();
+        microphone = new Microphone(handler, callback);
+
         textView = (TextView) findViewById(R.id.freq_text);
         textView.setTextSize(40);
         textView.setText("No audio");
-    }
 
-    /** Called when the user clicks the Send button */
-    public void startMicrophone(View view) {
-        textView.setText("Button pressed!");
-        mic.startRecording();
-        buffer = new byte[BUFFER_SIZE];
-        int bytes;
-        int i = 0;
-        while ((bytes = mic.read(buffer, 0, BUFFER_SIZE)) > 0) {
-            textView.setText(Double.toString(getFrequency(buffer)));
-            i++;
-            if (i > 100) break;
-        }
-    }
-
-    private double getFrequency(byte[] bytes) {
-        Log.d(TAG, "Getting frequency");
-        double[] samples = new double[bytes.length];
-        for (int i=0; i<bytes.length; i++) {
-            samples[i] = bytes[i];
-        }
-        Log.d(TAG, "Performing FFT");
-        doFFT(samples);
-        Log.d(TAG, "Finding max");
-        double max_amplitude = Double.NEGATIVE_INFINITY;
-        double max_freq = Double.NEGATIVE_INFINITY;
-        for (int i=0; i<samples.length; i++) {
-            if (samples[i] > max_amplitude) {
-                max_freq = SAMPLE_RATE * i / samples.length;
-                max_amplitude = samples[i];
-            }
-        }
-        Log.d(TAG, "Done: " + max_freq);
-        return max_freq;
-    }
-
-    // The variables and method below were taken almost verbatim from StackOverflow:
-    // http://stackoverflow.com/questions/4843739/audiorecord-object-not-initializing
-    private int[] sampleRates = new int[] { 8000, 11025, 22050, 44100 };
-    private short[] encodings =
-        new short[] { AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT };
-    private short[] channels =
-        new short[] { AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO };
-    public AudioRecord findMicrophoneSettings() {
-        for (int rate : sampleRates) {
-            for (short encoding : encodings) {
-                for (short channel : channels) {
-                    try {
-                        Log.d(TAG, "Attempting rate " + rate + "Hz, bits: " + encoding +
-                                ", channel: " + channel);
-                        int bufferSize = AudioRecord.getMinBufferSize(rate, channel, encoding);
-
-                        if (bufferSize != AudioRecord.ERROR_BAD_VALUE) {
-                            // check if we can instantiate and have a success
-                            AudioRecord recorder = new AudioRecord(
-                                    MediaRecorder.AudioSource.DEFAULT, rate, channel, encoding,
-                                    bufferSize);
-                            if (recorder.getState() == AudioRecord.STATE_INITIALIZED)
-                                return recorder;
-                        }
-                    } catch (Exception e) {
-                        Log.e(TAG, rate + "Exception, keep trying.",e);
-                    }
+        micOn = getResources().getString(R.string.mic_on);
+        micOff = getResources().getString(R.string.mic_off);
+        switchMic = (Button) findViewById(R.id.switch_mic);
+        switchMic.setOnClickListener(new View.OnClickListener() {
+            boolean start = true;
+            public void onClick(View view) {
+                switchMicState(start);
+                if (start) {
+                    start = false;
+                } else {
+                    start = true;
                 }
             }
-        }
-        return null;
+        });
+
+        renderer = new XYMultipleSeriesRenderer();
+        renderer.setYAxisMin(0.0);
+        renderer.setYAxisMax(10000.0);
+        seriesRenderer = new XYSeriesRenderer();
+        renderer.addSeriesRenderer(seriesRenderer);
+        freqData = new XYSeries("Frequency Magnitudes");
+        dataset = new XYMultipleSeriesDataset();
+        dataset.addSeries(freqData);
+        graphicalView = ChartFactory.getLineChartView(this, dataset, renderer);
+        graphicalView.refreshDrawableState();
+        graphicalView.repaint();
+        LinearLayout layout = (LinearLayout) findViewById(R.id.freq_graph);
+        layout.addView(graphicalView);
     }
 
-    private void doFFT(double[] samples) {
-        fft.realForward(samples);
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 
     @Override
     public void onPause() {
-        mic.release();
+        super.onPause();
+        stopMicrophone();
+    }
+
+    public void switchMicState(boolean start) {
+        if (start) {
+            switchMic.setText(micOff);
+            startMicrophone();
+        } else {
+            switchMic.setText(micOn);
+            stopMicrophone();
+        }
+    }
+
+    public void startMicrophone() {
+        microphone.start();
+    }
+
+    public void stopMicrophone() {
+        microphone.end();
+        microphone.interrupt();
+        microphone = new Microphone(handler, callback);
+    }
+
+    private void updateMaxFrequency() {
+        textView.setText(Double.toString(microphone.getCurrentFrequency()));
+    }
+
+    private void updateFreqGraph() {
+        freqData.clear();
+        double[] currentMags = microphone.getCurrentMags();
+        int num_bins = currentMags.length / 2;
+        for (int i=0; i<num_bins; i++) {
+            freqData.add(currentMags[2*i], currentMags[2*i+1]);
+        }
+        graphicalView.repaint();
+    }
+
+    private class FrequencyUpdater implements Runnable {
+        @Override
+        public void run() {
+            updateMaxFrequency();
+            updateFreqGraph();
+        }
+
     }
 }
